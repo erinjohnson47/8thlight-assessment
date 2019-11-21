15e3c1fb768f4fbc7ca17138d363942a6b6aa025
Erin Johnson <erinjohnson47@gmail.com>
2019-11-21 14:50:05 -0800

    takes user input from results and saves selected results to array, not yet available in reading list

--- src/cmds/search.js
+++ src/cmds/search.js
@@ -1,5 +1,14 @@
 const getBooks = require('../utils/api')
 const help = require('./help')
+const list = require('./list')
+
+const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+const booksForReadingList = [];
 
 module.exports = async (args) => {
     if (!args.hasOwnProperty('keywords')) {
@@ -9,17 +18,38 @@ module.exports = async (args) => {
     try {
         const keywords = args.keywords || args.k
         const books = await getBooks(keywords)
-        
-        // const booksMap = await books.map((element, index) => {
-        //     if (element.volumeInfo.authors === undefined || element.volumeInfo.authors.length === 1) {
-        //         return `${index+1}. Title: ${element.volumeInfo.title}, Author: ${element.volumeInfo.authors || 'none listed'}, Publisher: ${element.volumeInfo.publisher}`
-        //     } else {
-        //         let authors = element.volumeInfo.authors.map((a)=> ` ${a}` )
-        //         return `${index+1}. Title: ${element.volumeInfo.title}, Authors: ${authors}, Publisher: ${element.volumeInfo.publisher}`
-        //     }
-        // })
+ 
         console.log(`Books matching "${keywords}":`)
         console.log(books)
+        rl.question(`If you would like to save any of these books to your reading list, please enter the "Result" number(s) (separated by ',') and hit enter/return.  `, (answer) => {
+            let answerArr = answer.split(',')
+            for (i=0; i < answerArr.length; i++) {
+                switch (answerArr[i]) {
+                    case '1':
+                        booksForReadingList.push(books[0])
+                        break;
+                    case '2':
+                        booksForReadingList.push(books[1]);
+                        break;
+                    case '3':
+                        booksForReadingList.push(books[2]);
+                        break;
+                    case '4':
+                        booksForReadingList.push(books[3]);
+                        break;
+                    case '5':
+                        booksForReadingList.push(books[4])
+                        break;
+                    default:
+                        'None of these results have been saved to the reading list. Please search again if you\'d like.'
+                }
+            }
+            rl.close();
+            console.log(booksForReadingList)
+            console.log((booksForReadingList.length > 1) ? 
+                '^ These books have been saved to your reading list. To view your reading list, type \'books list\'.' : 
+                '^ This book has been saved to your reading list. To view your reading list, type \'books list\'.')
+        })
     } catch (err) {
         console.error(err)
     }
