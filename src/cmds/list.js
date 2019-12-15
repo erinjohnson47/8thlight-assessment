53d169f92933106e5611c72218983f51897dc0cf
Erin Johnson <erinjohnson47@gmail.com>
2019-12-15 14:11:45 -0800

    reformat reading list books

--- src/cmds/list.js
+++ src/cmds/list.js
@@ -1,7 +1,17 @@
 let readingList = [];
 
 module.exports = () => {
-    console.log('Your Reading List:')
-    console.log(readingList)
+    console.log(`Your Reading List:`)
+    const loopThroughReadingListArray = () => {
+        for (let i = 0; i < readingList.length; i++) {
+            readingList[i].map(book => {
+                console.log(`
+    Title: ${book.Title}
+    Author(s): ${book.Authors}
+    Publisher: ${book.Publisher}`)
+            })
+        }
+    }
+    loopThroughReadingListArray();
     process.exit()
 }
--- src/cmds/search.js
+++ src/cmds/search.js
@@ -1,6 +1,6 @@
 const getBooks = require('../utils/api')
 const help = require('./help')
-const fs = require('fs')
+const fileSystem = require('fs')
 
 //to accept user input in command line
 const readline = require('readline');
@@ -29,13 +29,12 @@ module.exports = async (args) => {
             const keywords = args.keywords || args.k
             const books = await getBooks(keywords)
             console.log(`Books matching "${keywords}":`)
-            const booksMap = await books.map((book) => {
+            await books.map((book) => {
                 console.log(`
-    Result: ${book.Result},
-    Title: ${book.Title},
-    Authors: ${book.Authors},
-    Publisher: ${book.Publisher}
-                `)
+    Result: ${book.Result}
+    Title: ${book.Title}
+    Author(s): ${book.Authors}
+    Publisher: ${book.Publisher}`)
             })
             //after displaying books, ask if user would like to save any of the results to reading list
             rl.question(`If you would like to save any of these books to your reading list, please enter the corresponding Result number(s).  `, (answer) => {
@@ -73,29 +72,28 @@ module.exports = async (args) => {
 
                 //if the readinglist array contains items:
                 if (booksForReadingList.length) {
-                    //read the existing list file
-                    fs.readFile(filePath, (err, data) => {
-                        if (err) {
-                            throw err;
-                        }
-                        let fileContent = data.toString();
-                        fileContent = fileContent.substring(position);
-                        //open file, r+ = for reading and writing
-                        let file = fs.openSync(filePath,'r+');
-                        //add exising file content to new file content so it is not overwritten
-                        let buffer = Buffer.from(`${JSON.stringify(booksForReadingList)},${fileContent}`);
-                        //write file specifying file, content, length of content, and position of where to write new conent
-                        fs.writeSync(file, buffer, 0, buffer.length, position);
-                    });
-                        //alert user that their choices have been saved to reading list
-                        const booksForListMap = booksForReadingList.map((book) => {
-                            console.log(`
-    Title: ${book.Title},
-    Authors: ${book.Authors},
-    Publisher: ${book.Publisher}
-                            `)
+                    console.log(booksForReadingList,'this is booksForReadingList')
+                    let booksJson = JSON.stringify(booksForReadingList)
+                    console.log(booksJson,'this is booksJson')
+
+                        //read the existing list file
+                        fileSystem.readFile(filePath, (err, data) => {
+                            if (err) {
+                                throw err;
+                            }
+                            let fileContent = data.toString();
+                            fileContent = fileContent.substring(position);
+                            //open file, r+ = for reading and writing
+                            let file = fileSystem.openSync(filePath,'r+');
+                            
+                            //add exising file content to new file content so it is not overwritten
+                            let buffer = Buffer.from(`${(booksJson)}, ${fileContent}`);
+                                    //write file specifying file, content, length of content, and position of where to write new conent
+                            fileSystem.writeSync(file, buffer, 0, buffer.length, position);
+                                    //alert user that their choices have been saved to reading list
                         });
-                        (booksForReadingList.length > 1) ? 
+
+                    (booksForReadingList.length > 1) ? 
                             console.log('^ These books have been saved to your reading list. To view your reading list, type \'books list\'.')
                             : console.log('^ This book has been saved to your reading list. To view your reading list, type \'books list\'.')
                 } else {
